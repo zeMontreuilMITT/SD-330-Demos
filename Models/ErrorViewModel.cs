05b1fd3220f5633b41abd477fc78908a581fb755
zeMontreuilMITT <zacharie.montreuil@mitt.ca>
2023-03-02 09:18:12 -0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SD_330_Demos.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
